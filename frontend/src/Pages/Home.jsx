282ae8707ef3db4e9bb357513a7cd3ee165919a3
Ahmed Ali <ahmedalinicobar@gmail.com>
2024-04-23 14:54:12 +0500

    Corrected buttons

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './app.css';
-import logo from '../Components/swapp.png'
-import x from '../Components/1.png'
+import "./app.css";
+import logo from "../Components/swapp.png";
+import x from "../Components/1.png";
 function Home() {
     return (
         <div class="cont">
@@ -11,21 +11,28 @@ function Home() {
             </div>
             <div class="box1">
                 <div class="box2">
-                
-                    <div class="lgbox"><img class="lg" src={logo}></img></div>
-                    
-                    <h2 id='h'>Getting The Right Courses, Easier Than Ever !</h2>
-                    
+                    <div class="lgbox">
+                        <img class="lg" src={logo}></img>
+                    </div>
+
+                    <h2 id="h">
+                        Getting The Right Courses, Easier Than Ever !
+                    </h2>
+
                     <div class="box3">
-                        <button class='purplebtn'>
-                            <Link to="/signin">Sign In</Link>
-                        </button>
-                        <hr style={{
-                            width: "5rem"
-                        }} />
-                        <button class='purplebtn' id ="up" Link to="/signup">
-                            <Link to="/signup">Sign Up</Link>
-                        </button>
+                        <Link to="/signin">
+                            <button class="purplebtn">Sign In</button>
+                        </Link>
+                        <hr
+                            style={{
+                                width: "5rem",
+                            }}
+                        />
+                        <Link to="/signup">
+                            <button class="purplebtn" id="up" Link to="/signup">
+                                Sign Up
+                            </button>
+                        </Link>
                     </div>
                 </div>
             </div>
